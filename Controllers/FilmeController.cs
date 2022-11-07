5171091aa2cbc8efd4b8f4d754ecfc282cb5c0fc
John Duque <duquejohn185@gmail.com>
2022-11-06 23:52:18 -0300

    inserindo o _context no meu FilmeController

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc; // Serve para conseguirmos usar algumas anotações expecificas do http
 using FilmesAPI.models;
+using FilmesAPI.Data; 
 
 namespace FilmesAPI.Controllers
 {
@@ -12,24 +13,32 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
 
+        private FilmeContext _contenxt;
+
+        public FilmeController(FilmeContext contenxt)
+        {
+            _contenxt = contenxt;
+        }
 
         [HttpPost] // serve para expecificar o metedo que sera usado  
         public IActionResult AdicionaFilme([FromBody] Filme filme) //FromBody Serve para usar os dados que seram enviados pelo meu body 
         {
+            _contenxt.Add(filme);
             return CreatedAtAction(nameof(recuperarFilmePorId), new { Id = filme.Id}, filme);
         }
 
         [HttpGet]
         public IActionResult recuperaFilmes()
         {
-            return Ok();
+            //ira returnar todo o conjunto de dados de filme
+            return Ok(_contenxt.Filmes);
         }
 
         [HttpGet("{id}")] //PPara conseguirmos colocar params na URL
         public IActionResult recuperarFilmePorId(int id)
         {
             //O FirstOrDefault serve para retonar o primeiro filme que contenha o id expecificado pelo cliente
-            Filme filme = .FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _contenxt.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if (filme != null)
             {
